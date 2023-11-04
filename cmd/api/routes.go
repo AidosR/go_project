3b442d9af4a09f380ddc3fd4a7a114f1cd5f0af0
AidosR <a_rustem@kbtu.kz>
2023-11-04 23:47:48 +0600

    Ch10 added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
 
@@ -19,5 +19,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/play_tents/:id", app.updatePlayTentHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/play_tents/:id", app.deletePlayTentHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
