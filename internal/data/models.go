577cde8025f413fb494c545a986df09d8e31cc43
AidosR <a_rustem@kbtu.kz>
2023-11-27 14:09:34 +0600

    Ch15 added

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,12 +12,14 @@ var (
 
 type Models struct {
 	PlayTents PlayTentModel
+	Tokens    TokenModel
 	Users     UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		PlayTents: PlayTentModel{DB: db},
+		Tokens:    TokenModel{DB: db},
 		Users:     UserModel{DB: db},
 	}
 }
